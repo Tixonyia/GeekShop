9b7cee75ce596b336b0d1cd788c991fbb2fa4c2a
Tihonyia <220220220220@rambler.ru>
2020-12-20 19:24:39 +0300

    потерял репозиторий востонавливаю

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,22 +2,29 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=73, unique=True)
+    name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=333)
+    name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='products_images', blank=True)
     description = models.TextField(blank=True)
-    short_description = models.CharField(max_length=77, blank=True)
+    short_desc = models.CharField(max_length=64, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f'{self.name} {self.category.name}'
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
